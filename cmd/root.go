47e1b797673cef4d34f6cc7d5cb21c6001b4315f
Toon van Strijp <toonvanstrijp@gmail.com>
2020-02-13 23:16:24 +0100

    initial init

--- cmd/root.go
+++ cmd/root.go
@@ -20,7 +20,6 @@ import (
 	"github.com/spf13/cobra"
 	"os"
 
-	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/viper"
 )
 
@@ -70,16 +69,9 @@ func initConfig() {
 		// Use config file from the flag.
 		viper.SetConfigFile(cfgFile)
 	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
 		// Search config in home directory with name ".kip" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".kip")
+		viper.AddConfigPath(".")
+		viper.SetConfigName("kip_config")
 	}
 
 	viper.AutomaticEnv() // read in environment variables that match
